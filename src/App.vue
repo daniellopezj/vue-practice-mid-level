e327c93caa14323a03bbd6ab79a30b161e0654cc
daniel <daniellopezj0327@gmail.com>
2025-04-02 15:05:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+import WatchTest from './components/WatchTest.vue'
+import ComputedTest from './components/ComputedTest.vue'
+</script>
+
+<template>
+<div>
+  <WatchTest />
+  <ComputedTest />
+</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ComputedTest.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+const fullName = computed({
+  get() {
+    return firstName.value + ' jhjjh ' + lastName.value
+  },
+  set(newValue) {
+    [firstName.value, lastName.value] = newValue.split(' ')
+  }
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    fullName.value = 'una prueba'
+  }, 5000);
+})
+</script>
+
+<template>
+<div>
+  {{  fullName }}
+</div>
+</template>
